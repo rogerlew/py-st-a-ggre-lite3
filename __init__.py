3fdb8431b62afc9c9bcd8b72f9a52033fc46e9a2
roger <roger@bearnote>
2011-05-09 16:36:35 -0700

    setup added

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2011, Roger Lew [see LICENSE.txt]
+# This software is funded in part by NIH Grant P20 RR016454.
+
+<root>/
+    setup.py
+    pystaggrelite3.py
+    test__pystaggrelite3.py
+    example__pystaggrelite3.py
+
+from pystaggrelite3 import pystaggrelite3
